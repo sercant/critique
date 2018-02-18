4748aa0a9e123f677f5efbc0cbaeb19d2a807b4e
Sercan Turkmen <sercanturkmen@outlook.com>
2018-02-18 19:12:28 +0200

    schema update

--- db/forum_schema_dump.sql
+++ db/forum_schema_dump.sql
@@ -4,12 +4,12 @@ BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS users(
   user_id INTEGER PRIMARY KEY AUTOINCREMENT,
   nickname TEXT UNIQUE,
-  regDate INTEGER,
+  regDate INTEGER NOT NULL,
   lastLogin INTEGER);
 
 CREATE TABLE IF NOT EXISTS users_profile(
   user_id INTEGER PRIMARY KEY,
-  firstname TEXT,
+  firstname TEXT NOT NULL,
   lastname TEXT,
   bio TEXT,
   email TEXT UNIQUE,
@@ -20,26 +20,25 @@ CREATE TABLE IF NOT EXISTS users_profile(
   FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE);
 
 CREATE TABLE IF NOT EXISTS posts (
-  post_id INTEGER,
-  timestamp INTEGER,
-  public INTEGER,
-  sender_id INTEGER,
-  receiver_id INTEGER,
+  post_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  timestamp INTEGER NOT NULL,
+  public INTEGER NOT NULL,
+  sender_id INTEGER NOT NULL,
+  anonymous INTEGER NOT NULL,
+  receiver_id INTEGER NOT NULL,
   reply_to TEXT,
-  post_text TEXT,
+  post_text TEXT NOT NULL,
   rating INTEGER,
-  PRIMARY KEY(post_id),
   FOREIGN KEY(sender_id) REFERENCES users(user_id) ON DELETE CASCADE,
-  FOREIGN KEY (receiver_id) REFERENCES users(user_id) ON DELETE CASCADE,
+  FOREIGN KEY(receiver_id) REFERENCES users(user_id) ON DELETE CASCADE,
   FOREIGN KEY(reply_to) REFERENCES posts(post_id) ON DELETE CASCADE);
 
 CREATE TABLE IF NOT EXISTS ratings(
-  ratings_id INTEGER PRIMARY KEY,
-  timestamp INTEGER,
-  rating INTEGER,
-  sender_id INTEGER,
-  receiver_id INTEGER,
-  PRIMARY KEY(ratings_id),
+  ratings_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  timestamp INTEGER NOT NULL,
+  rating INTEGER NOT NULL,
+  sender_id INTEGER NOT NULL,
+  receiver_id INTEGER NOT NULL,
   FOREIGN KEY(sender_id) REFERENCES users(user_id) ON DELETE CASCADE,
   FOREIGN KEY(receiver_id) REFERENCES users(user_id) ON DELETE CASCADE);
 
